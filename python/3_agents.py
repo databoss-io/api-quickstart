248006ff59b2a392cfbed41c1b0e054db895dbf3
Nathan Herrington <Nathanmanforever@gmail.com>
2025-01-22 10:22:51 -0700

    Adding agent example

---
+++ python/3_agents.py
@@ -0,0 +1,49 @@
+from os import getenv
+import requests
+import json
+
+# Get our environment configuration -------------------------------------------
+
+# Windows: you can use `set DATABOSS_CLIENT_ID=<value>` to set the environment
+# Linux/macOS: you can use `export DATABOSS_CLIENT_ID=<value>` to set the environment
+# Repeat the same with the DATABOSS_CLIENT_SECRET
+CLIENT_ID = getenv("DATABOSS_CLIENT_ID")
+CLIENT_SECRET = getenv("DATABOSS_CLIENT_SECRET")
+ORGANIZATION_ID = getenv("DATABOSS_ORG_ID")
+
+# Get access token ------------------------------------------------------------
+
+
+get_token = requests.post("https://login.microsoftonline.com/databossio.onmicrosoft.com/oauth2/v2.0/token", data={
+    "grant_type": "client_credentials",
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET,
+    "scope": "https://databossio.onmicrosoft.com/api/.default"
+})
+
+# Handle if getting the token failed
+try:
+    get_token.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    err.add_note(get_token.text)
+    raise err
+
+# Get token value
+get_token_resp = get_token.json()
+access_token = get_token_resp.get("access_token")
+if not access_token:
+    raise ValueError("Token not returned!")
+
+# Perform test call -----------------------------------------------------------
+
+agents = requests.get(f"https://api-prod.databoss.io/v1/organization/{ORGANIZATION_ID}/agent", headers={
+    "Authorization": f"Bearer {access_token}"
+})
+agents.raise_for_status()
+agents_resp = agents.json()
+
+# Final output ----------------------------------------------------------------
+
+print("\r\n****** Associated Agents *******")
+print(json.dumps(agents_resp.get('result'), indent=2))
+print("***********************")
